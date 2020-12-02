7e2b6712dfe71119a83904a6a07779024748be8e
ashelley <ashelley@gmail.com>
2020-12-01 19:58:48 -0800

    actually allow you to switch scenes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ export default class App extends React.Component {
             <div style={{ display:'flex', position:'absolute', top:0,left:0,right:0,bottom:0, backgroundColor:'#404040', alignItems:'center', justifyContent:'center'}}>
                 <SceneSelector />
             </div>
+        )
     }
 
 }
--- src/SceneSelector.tsx
+++ src/SceneSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useCallback, useState } from "react";
 import TestSoftwareLineDrawing from "./software_scenes/TestSoftwareLineDrawing";
 import TestWireFrame from "./software_scenes/TestWireFrame";
 import TriangleRasterization from "./software_scenes/TriangleRasterization";
@@ -14,26 +14,59 @@ import TriangleRasterizationScratch from "./software_scenes/TriangleRasterizatio
 import PerspectiveRasterizationScratchZBuffer from "./software_scenes/PerspectiveRasterizationScratchZBuffer";
 import GURU8_8 from "./software_scenes/GURU8_8";
 import GURU7_1 from './software_scenes/GURU7_1';
+import React from "react";
 
-export default class SceneSelector extends React.Component<{},{}> {
-    render() {
-        return (
-            //* <WebGLCanvas /> */}
-            // <TestSoftwareLineDrawing />
-            // <TestWireFrame />
-            //<TriangleRasterization />
-            //<TestShadedRandom />
-            //<TestSimpleShading />
-            // <TestZBuffer />
-            //<TestProjection />   //TODO: not complete
-            //<TestMaxtrixTransform />
-            //<TestRayTracer />
-            //<TestXTree />
-            //<TestPinHole />
-            //<TriangleRasterizationScratch />
-            //<PerspectiveRasterizationScratchZBuffer />
-            //<GURU7_1 />
-            <GURU8_8 />
-        )
+let scenes:{[name:string]:any} = {}
+
+scenes['guru8_8'] = {Component: GURU8_8, label: 'Guru 8-8'}
+scenes['guru7_1'] = {Component: GURU7_1, label: 'Guru 7-1'}
+
+function SceneOption({scene,setScene}:{scene:string,setScene:(name:string) => void}) {
+
+    let options = []
+    for(let key in scenes) {
+        let info = scenes[key]
+        let {label} = info
+        options.push(<option key={key} value={key}>{label}</option>)
     }
+
+    let onSceneChange = useCallback((e) => {
+        setScene(e.target.value)
+    },[])
+
+    return (
+        <select value={scene} onChange={onSceneChange}>
+            {options}
+        </select>
+    )
+}
+
+export default function SceneSelector() {
+
+    let [scene,setScene] = useState<string>('guru8_8')
+
+    let {Component} = scenes[scene]
+
+    return (
+        //* <WebGLCanvas /> */}
+        // <TestSoftwareLineDrawing />
+        // <TestWireFrame />
+        //<TriangleRasterization />
+        //<TestShadedRandom />
+        //<TestSimpleShading />
+        // <TestZBuffer />
+        //<TestProjection />   //TODO: not complete
+        //<TestMaxtrixTransform />
+        //<TestRayTracer />
+        //<TestXTree />
+        //<TestPinHole />
+        //<TriangleRasterizationScratch />
+        //<PerspectiveRasterizationScratchZBuffer />
+        <>
+        <div style={{position:'absolute', top: 0, right: 0}}>
+            <SceneOption scene={scene} setScene={setScene} />
+        </div>
+        <Component />
+        </>
+    )
 }
